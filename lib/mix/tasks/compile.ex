32218508b42a28c0a1e93c59095a262ae811a4ba
Anthony Grimes <i@raynes.me>
2012-04-16 19:37:26 -0700

    Update for the latest elixir.
    Just meant updating the compile task to use the new global compiler
    options.

--- lib/mix/tasks/compile.ex
+++ lib/mix/tasks/compile.ex
@@ -50,20 +50,20 @@ defmodule Mix.Tasks.Compile do
     project = Mix.Mixfile.get_project
     compile_path = project[:compile_path]
     compile_first = project[:compile_first]
-    options = project[:compile_options]
+    Code.compiler_options(project[:compile_options])
     :file.make_dir compile_path
     to_compile = extract_files(project[:source_paths])
     if force == "--force" || Enum.find(to_compile, stale?(&1, compile_path)) do
       if !Enum.empty?(compile_first) do
         IO.puts "\nPerforming initial compilation (compile_first)...\n"
-        Enum.each(compile_first, compile_file(&1, compile_path, options))
+        Enum.each(compile_first, compile_file(&1, compile_path))
         IO.puts "\nCompiling everything else...\n"
       end
       Enum.each(project[:source_paths], fn(path) ->
         files = File.wildcard(File.join([path, "**/*.ex"]))
         Enum.each(files, fn(file) ->
           if !Enum.find(project[:compile_first], fn(x) -> x == file end) do
-            compile_file(file, compile_path, options)
+            compile_file(file, compile_path)
           end
         end)
       end)
@@ -75,9 +75,9 @@ defmodule Mix.Tasks.Compile do
     List.concat(lc path in paths, do: File.wildcard(File.join([path, "**/*.ex"])))
   end
 
-  defp compile_file(file, to, options) do
+  defp compile_file(file, to) do
     IO.puts Enum.join(["Compiling ", file, " to ", to, "..."])
-    Code.compile_file_to_dir(file, to, options)
+    Code.compile_file_to_dir(file, to)
   end
 
   defp stale?(file, to) do
