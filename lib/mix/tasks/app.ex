f36b2a973335d8c66ab54ba2c442785a2d167048
Yurii Rashkovskii <yrashk@gmail.com>
2012-05-28 00:13:44 -0700

    Makes 'app' task figure out the list of modules automatically

--- lib/mix/tasks/app.ex
+++ lib/mix/tasks/app.ex
@@ -6,11 +6,17 @@ defmodule Mix.Tasks.App do
        project = Mix.Mixfile.get_project
        app_name_atom = list_to_atom(LC.to_char_list project[:project])
        app_name = LC.to_char_list app_name_atom
-       best_guess_app = [vsn: (LC.to_char_list project[:version])]
+       compile_path = project[:compile_path]
+       modules = 
+       lc file in File.wildcard("#{compile_path}/**/*.beam") do
+          s = Regex.replace_all(%r/\//, Regex.replace(%r/ebin\/__MAIN__\/(.+)\.beam/, file, "\\1"), ".")
+          Module.concat [s]
+       end
+
+       best_guess_app = [vsn: (LC.to_char_list project[:version]), modules: modules]
        app = {:application, app_name_atom, Keyword.merge best_guess_app, Mix.Project.application}
 
        # write down the .app file
-       compile_path = project[:compile_path]
        {:ok, f} = :file.open(File.join([compile_path, "#{app_name}.app"]),[:write])
        :io.fwrite(f, "~p.", [app])
        :file.close(f)
